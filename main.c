ffb399bccd1cbd55c4eeba531d06996816148ee3
aliavang <714@holbertonschool.com>
2019-03-20 03:54:20 +0000

    Main file to test, printf line fix, line 24

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+}
