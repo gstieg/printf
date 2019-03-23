28182029ec4d5ab8b03d6eed846a58802a4990ea
zinczar <brendan@wineisneat.com>
2019-03-23 20:03:57 +0000

    removed sandbox from .gitignore

---
+++ sandbox/get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * get_op_func - Checks format specifier
+ *
+ * @s: Format specifier
+ *
+ * Return: result or NULL
+ */
+int (*get_op_func(char s))(va_list arguments)
+{
+	specifier_t specifier_array[] = {
+	{'c', print_char},
+	{'s', print_string},
+/*	{'%', print_percent},*/
+	{'\0', NULL}
+	};
+
+	int i = 0;
+
+	while (specifier_array[i].s)
+	{
+		if (s == specifier_array[i].s)
+		{
+			return (specifier_array[i].function_ptr);
+		}
+		i++;
+	}
+	return (0);
+}
